4052f1396212f0781ebf96713e50af3b95c8b1d1
lenryk <lenry101@gmail.com>
2017-12-16 23:35:45 +0000

    added form and incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,21 @@ socket.on("disconnect", function() {
 
 socket.on("newMessage",function(msg) {
    console.log(msg.from +" says: " +msg.text);
+   let li = $("<li></li>");
+   li.text(`${msg.from}: ${msg.text}`);
+
+   $("#messages").append(li);
+});
+
+$("#message-form").on("submit", function (e) {
+    e.preventDefault();
+
+    socket.emit("createMessage", {
+        from: "User",
+        text: $("[name=message]").val()
+    }, function () {
+
+    });
 });
 
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
+const {generateMessage} = require("./utils/message");
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,"../public");
 let app = express();
@@ -14,29 +15,14 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("New user");
 
-    socket.emit("newMessage", {
-        from: "Admin",
-        text: "Welcome to the chat app"
-    });
+    socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app"));
 
-    socket.broadcast.emit("newMessage", {
-        from: "Admin",
-        text: "New user joined",
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit("newMessage", generateMessage("Admin", "New user joined"));
 
-    socket.on("createMessage", (message) => {
+    socket.on("createMessage", (message,callback) => {
        console.log(message);
-     /*  io.emit("newMessage", {
-           from: message.from,
-           text: message.text,
-           createdAt: new Date().getTime()
-       });*/
-        socket.broadcast.emit("newMessage", {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit("newMessage", generateMessage(message.from,message.text));
+        callback("This is from the server");
     });
 
     socket.on("disconnect", () => {
