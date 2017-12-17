53d281e8578846303c03212be4c8580bda82ae51
lenryk <lenry101@gmail.com>
2017-12-17 20:43:13 +0000

    scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,20 @@ socket.on("disconnect", function() {
     console.log("Disconnected from server");
 });
 
+function scrollToBottom() {
+    let messages = $("#messages");
+    let newMessage = messages.children("li:last-child");
+    let clientHeight = messages.prop("clientHeight");
+    let scrollTop = messages.prop("scrollTop");
+    let scrollHeight = messages.prop("scrollHeight");
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on("newMessage",function(msg) {
     let formattedTime = moment(msg.createdAt).format("h:mm a");
     let template = $("#message-template").html();
@@ -17,6 +31,7 @@ socket.on("newMessage",function(msg) {
     });
 
     $("#messages").append(html);
+    scrollToBottom();
 });
 
 socket.on("newLocationMessage", function(message) {
@@ -30,6 +45,7 @@ socket.on("newLocationMessage", function(message) {
 
 
    $("#messages").append(html);
+    scrollToBottom();
 });
 
 $("#message-form").on("submit", function (e) {
