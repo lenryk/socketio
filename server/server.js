8803da8fb2dc93f5db4da1c7eaa29d4e3ccbd46a
lenryk <lenry101@gmail.com>
2017-12-18 16:36:05 +0000

    first release

--- server/server.js
+++ server/server.js
@@ -34,14 +34,22 @@ io.on("connection", (socket) => {
     });
 
     socket.on("createMessage", (message,callback) => {
-       console.log(message);
-        io.emit("newMessage", generateMessage(message.from,message.text));
+        let user = users.getUser(socket.id);
+
+        if(user && isRealString(message.text)) {
+            io.to(user.room).emit("newMessage", generateMessage(user.name,message.text));
+        }
+
         callback();
     });
 
     socket.on("createLocationMessage", (coords) => {
         let formattedTime = moment(coords.createdAt).format("h:mm a");
-        io.emit("newLocationMessage", generateLocationMessage(`Admin`,coords.latitude,coords.longitude));
+        let user = users.getUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit("newLocationMessage", generateLocationMessage(user.name,coords.latitude,coords.longitude));
+        }
     });
 
     socket.on("disconnect", () => {
